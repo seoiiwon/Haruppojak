32de59a4f65d5f77a49dae525f5f5e7950e9cee1
Gromit-IT <lwc980529@gnu.ac.kr>
2024-07-25 17:00:23 +0900

    라우터 수정3

--- Server/api/MainRouter.py
+++ Server/api/MainRouter.py
@@ -15,22 +15,40 @@ template_dir = os.path.join(os.path.dirname(
     __file__), "../../Web/templates/AuthPage")
 templates = Jinja2Templates(directory=template_dir)
 
-# todo 리스트 보기
 
-
-@router.get("/todo/all", response_class=HTMLResponse)
+@router.get("/todo/all", response_class=HTMLResponse)  # todo 리스트 보기
 async def read_todos(request: Request, db: Session = Depends(get_db)):
     todos = get_todos(db)
     return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
 
-# todo 리스트 만들기
 
-
+# todo 만들기
 @router.post("/todo/create", response_model=TodoListSchema.TodoCreate)
-async def create_todo(
+async def create_new_todo(
     todo: TodoListSchema.TodoCreate, db: Session = Depends(get_db)
 ):
     return create_todo(db=db, todo=todo)
 
 
+# todo 수정하기
+@router.put("/todo/update/{todo_id}", response_model=TodoListSchema.TodoUpdate)
+async def update_new_todo(
+    todo_id: int, todo: TodoListSchema.TodoUpdate, db: Session = Depends(get_db)
+):
+    return update_todo(db=db, todo_id=todo_id, todo_update=todo)
+
+
+# todo 삭제하기
+@router.delete("/todo/delete/{todo_id}", response_model=TodoListSchema.TodoList)
+async def delete_existing_todo(
+    todo_id: int, db: Session = Depends(get_db)
+):
+    return delete_todo(db=db, todo_id=todo_id)
+
+
+@router.check("/todo/check/{todo_id}", response_model=TodoListSchema.TodoCheck)
+async def check_existing_todo(
+    todo_id: int, todo: TodoListSchema.TodoCheck, db: Session = Depends(get_db)
+):
+    return check_todo(db=db, todo_id=todo_id, todo_check=todo)
 Main_router = router
