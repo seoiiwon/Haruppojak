b7065120e8836511bf61dc9be15a8225ed2f58c8
Seo Jiwon <wldnjstj99@gmail.com>
2024-07-25 15:30:29 +0900

    gg

--- Server/api/AuthRouter.py
+++ Server/api/AuthRouter.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from config.database import get_db
+import os
 
 router = APIRouter()
 
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
-<<<<<<< Updated upstream:Server/main.py
-from api import AuthRouter, CalendarRouter, MainRouter, DiaryRouter, ProfileRouter
-from models import TodoListModel
-from schemas import TodoListSchema
-from crud import MainCrud
-=======
 from Server.api.AuthRouter import auth_router
 from Server.api.MainRouter import Main_router
->>>>>>> Stashed changes:main.py
 app = FastAPI()
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="Web/static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,9 +15,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-<<<<<<< Updated upstream:Server/main.py
-app.include_router(router.router)
-=======
 app.include_router(auth_router)
 app.include_router(Main_router)
->>>>>>> Stashed changes:main.py
