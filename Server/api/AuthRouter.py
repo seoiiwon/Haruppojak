f12e4b265daacbcbb09a01572b374de294968ddc
Seo Jiwon <wldnjstj99@gmail.com>
2024-07-25 15:26:44 +0900

    gg

--- Server/api/AuthRouter.py
+++ Server/api/AuthRouter.py
@@ -4,4 +4,30 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from config.database import get_db
 
-router = APIRouter(prefix="/api")
+router = APIRouter()
+
+template_dir = os.path.join(os.path.dirname(__file__), "../../Web/templates/AuthPage")
+templates = Jinja2Templates(directory=template_dir)
+
+@router.get("/", response_class=HTMLResponse)
+async def getLoginMenuHaruPpojak(request: Request):
+    return templates.TemplateResponse(name="HaruPpojakLoginMenu.html", request=request)
+    # return templates.TemplateResponse("HaruPpojakLoginMenu.html", {"request" : request})
+
+@router.get("/auth/signin", response_class=HTMLResponse)
+async def getSignInPage(request : Request):
+    return templates.TemplateResponse("HaruPpojakSignIn.html", {"request" : request})
+
+@router.get("/auth/signup", response_class=HTMLResponse)
+async def getSignUpPage(request : Request):
+    return templates.TemplateResponse("HaruPpojakSignUp.html", {"request" : request})
+
+@router.get("/HaruPpojak/intro", response_class=HTMLResponse)
+async def getIntroPage(request : Request):
+    return templates.TemplateResponse("HaruPpojakIntroPage.html", {"request" : request})
+
+@router.get("/HaruPpojak/Cam", response_class=HTMLResponse)
+async def getUserCam(request : Request):
+    return templates.TemplateResponse("HaruPpojakCamera.html", {"request" : request})
+
+auth_router = router
