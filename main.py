9749156b969b741cb3096b4b95573717dfd6ad94
USER <codwix@naver.com>
2024-07-25 15:22:54 +0900

    update

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+from Server.api.AuthRouter import auth_router
+from Server.api.AuthRouter import DiaryRouter
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="Web/static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router)
+app.include_router(DiaryRouter)
