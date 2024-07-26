20f271bcb04bc46cd5e6a739c4ecf3b8fee57cf6
USER <codwix@naver.com>
2024-07-27 01:05:44 +0900

    test파일
    test파일 수정

--- Server/crud/DiaryCrud.py
+++ Server/crud/DiaryCrud.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
 from datetime import datetime
-from Server.models import DiaryModel
-from Server.schemas import DiarySchema
+from Server.models.DiaryModel import *
+from Server.schemas.DiarySchema import *
 
-def CreateDiary(db: Session, diary: DiarySchema.CreateDiarySchema):
-    diary = DiaryModel.UserDiary(content = diary.content,
+def CreateDiary(db: Session, diary: CreateDiarySchema):
+    dbdiary = UserDiary(content = diary.content,
                       date = datetime.now())
-    db.add(diary)
+    db.add(dbdiary)
     db.commit()
--- Server/userTest.py
+++ Server/userTest.py
@@ -1,10 +1,38 @@
-from Server.config.database import SessionLocal, engine
-from Server.models.UserModel import Base, UserInfo
-from Server.models.DiaryModel import UserDiary
+from config.database import SessionLocal, engine
+from models.UserModel import Base, UserInfo
+from models.DiaryModel import Base, UserDiary
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
 # Initialize the database
 Base.metadata.create_all(bind=engine)
 
 # Create a new session
 db = SessionLocal()
+
+try:
+    # Create a new user
+    diary = UserDiary(
+        Date=datetime.now(),
+        Diarycontent="김뽀작의 일기장",
+        Response="안녕 뽀짝아",
+        Diarytodo=""  # Ensure the role is in uppercase
+    )
+
+    # Add the user to the session
+    db.add(diary)
+    
+    # Commit the session
+    db.commit()
+    
+    # Refresh the user instance to get the ID
+    db.refresh(diary)
+
+    print(f"User {diary.Diarycontent} created with ID {diary.id}")
+
+except Exception as e:
+    db.rollback()
+    print(f"Error: {e}")
+
+finally:
+    db.close()
