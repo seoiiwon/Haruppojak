d5af87746537bc1cc04182d90f36e8b08c8b7df7
USER <codwix@naver.com>
2024-07-28 21:56:37 +0900

    update

--- Server/crud/DiaryCrud.py
+++ Server/crud/DiaryCrud.py
@@ -1,13 +1,17 @@
-# from sqlalchemy.orm import Session
-# from datetime import datetime
-# from Server.models.DiaryModel import *
-# from Server.schemas.DiarySchema import *
+from sqlalchemy.orm import Session
+from datetime import datetime
+from Server.models.DiaryModel import *
+from Server.schemas.DiarySchema import *
 
-# def CreateDiary(db: Session, diary: CreateDiarySchema):
-#     dbdiary = UserDiary(content = diary.content,
-#                       date = datetime.now(),
-#                       response=diary.response,
-#                       todo=diary.todo
-#                       )
-#     db.add(dbdiary)
-#     db.commit()
+def CreateDiary(db: Session, diary: CreateDiarySchema):
+    dbdiary = UserDiary(content = diary.content,
+                      date = datetime.now(),
+                      response=diary.response,
+                      todo=diary.todo
+                      )
+    db.add(dbdiary)
+    db.commit()
+
+def deleteDiary(db:Session, diary:UserDiary):
+    db.delete(diary)
+    db.commit()
