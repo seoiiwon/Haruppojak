a64335d5995d1445bcb665ee50211ef464f3ef2a
Seo Jiwon <wldnjstj99@gmail.com>
2024-08-06 03:41:36 +0900

    챌린지 참여 현황 수정

--- Server/api/ChallengeRouter.py
+++ Server/api/ChallengeRouter.py
@@ -64,6 +64,9 @@ async def postChallenge(request: Request,
     postNewChallenge(db, challenge, challengeThumbnail1,
                      challengeThumbnail2, challengeThumbnail3)
     challengeListAll = getChallengeListAll(db)
+    challengers = []
+    for challenge in challengeListAll:
+        challenger = challengers.append()
     return templates.TemplateResponse(name="ChallengeList.html", context={"request": request, "challengeList": challengeListAll})
 
 
@@ -73,5 +76,5 @@ async def joinNewChallenge(challenge_request: ChallengeSchema.ChallengeJoinReque
                            user: AuthSchema.UserInfoSchema = Depends(
                                getCurrentUser),
                            db: Session = Depends(get_db)):
-    joinChallenge(challenge_id=challenge_request.challenge_id,
-                  current_user=user, db=db)
+    joinChallenge(challenge_id=challenge_request.challenge_id,current_user=user, db=db)
+    
