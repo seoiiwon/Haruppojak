eaa5eaafc8064cf5693d8d86f9bb2236ad16c6c7
Seo Jiwon <134384128+seoiiwon@users.noreply.github.com>
2024-08-09 13:07:28 +0900

    Delete joinedChallengeTest.py

--- joinedChallengeTest.py
+++
@@ -1,9 +0,0 @@
-from Server.schemas.ChallengeSchema import UserChallenge
-from Server.config.database import SessionLocal
-from Server.models.UserModel import UserChallenge
-
-
-Session = SessionLocal()
-
-print(Session.query(UserChallenge).filter(UserChallenge.user_id == 2).all())
-
